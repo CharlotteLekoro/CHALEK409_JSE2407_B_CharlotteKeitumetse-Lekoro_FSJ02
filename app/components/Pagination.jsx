e8e06ed5993a95f24c94181154c0875120b3ad57
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-10-01 06:10:36 +0200

    editted the colour on store

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -18,14 +18,14 @@ export default function Pagination({ currentPage, searchQuery, selectedCategory,
     <div className="flex justify-between items-center mt-8">
       {prevPage && (
         <Link href={buildUrl(prevPage)}>
-          <button className="px-4 py-2 bg-pink-400 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300">
+          <button className="px-4 py-2 bg-green-400 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300">
             Previous Page
           </button>
         </Link>
       )}
       <span className="text-lg font-bold text-gray-700">Page {pageNum}</span>
       <Link href={buildUrl(nextPage)}>
-        <button className="px-4 py-2 bg-pink-400 text-white rounded-lg hover:bg-purple-500 transition-colors duration-300">
+        <button className="px-4 py-2 bg-green-400 text-white rounded-lg hover:bg-gray-600 transition-colors duration-300">
           Next Page
         </button>
       </Link>
